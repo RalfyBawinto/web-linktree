da2f20c7e2f4e0ed62703d7d6e17bc5494687a75
Ralfy <briggith11@MacBook-Pro-Ralfy.local>
2025-02-22 20:05:41 +0800

    update transition bg jadi lebih smooth

---
+++ .history/script_20250222194748.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const backgrounds = document.querySelectorAll(".background-container .bg");
+  let currentIndex = 0;
+
+  setInterval(() => {
+    backgrounds[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % backgrounds.length;
+    backgrounds[currentIndex].classList.add("active");
+  }, 2500); // Ganti background setiap 5 detik
+});
